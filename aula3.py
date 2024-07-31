037da0b27c7e6dbaf88264372abe4c1363718d2b
gabrielcarliin <75861006+gabrielcarliin@users.noreply.github.com>
2024-07-30 22:11:54 -0300

    Aula 3 - Tipo str (string) - Introdução aos tipos de dados

---
+++ aula3.py
@@ -0,0 +1,20 @@
+"""
+Python = Linguagem  programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+# Aspas simples
+print('Gabriel Carlin')
+
+# Aspas duplas
+print("Gabriel 'Carlin'")
+
+# Escape
+print("Gabriel \"Carlin\"")
+
+#r
+print(r"Gabriel \"Carlin\"")
